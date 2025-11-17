4382d78a5ab9b99a372eb345c73eef12c8c8a8ee
KimaaDevJR <romaincappellaro@gmail.com>
2025-11-17 20:47:25 +0100

    Add files via upload

---
+++ JavaScript/BotsFCC/TriviaBotFCC/script.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "CP-313430";
+let botLocation = "the planet Foizqi";
+let favoriteLanguage ="C++";
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+let codingFact = "My Algorithm teacher loves " + favoriteLanguage + ".";
+console.log(codingFact);
+codingFact = "The grandfather of my Algorithm teacher who loves " + favoriteLanguage + " has a red bike.";
+console.log(codingFact);
+codingFact = "The favourite teacher of my Algorithm teacher who loves " + favoriteLanguage + " is Michelin";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + (botLocation) + ".");
---
+++ JavaScript/SentenceMaker/script.js
@@ -0,0 +1,18 @@
+let adjective = "great";
+let noun = "orc";
+let verb = "stimulated";
+let place = "gay bar";
+let adjective2 = "huge";
+let noun2 = "Robert";
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+adjective = "beautiful";
+noun = "centaur";
+verb = "sterilized";
+place = "Hogwarts";
+adjective2 = "marvelous";
+noun2 = "cats";
+let secondStory
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
